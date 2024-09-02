a323647a1fb67127c5fdcf73e601d3974c435d7f
Shubham <shubhamkumar98524@gmail.com>
2024-09-02 12:24:57 +0000

    Methos function in java

---
+++ main.java
@@ -0,0 +1,43 @@
+// public class main {
+//     public static void printJava(){
+//         System.out.println("Hello Java");
+//     }
+//     public static void main(String[] args) {
+           // Method Function
+           
+//         printJava(); // Hello Java
+//         printJava(); // Hello Java
+//         printJava(); // Hello Java
+//     }
+// }
+
+
+// public class main {
+//     public static void printJava() {
+//         System.out.println("Hello Java");
+//     }
+//     public static void printName(String name) {
+//         System.out.println(name);
+//     }
+//     public static void main(String[] args) {
+//         printName("Aman");  // Aman
+//         printName("Akku");  // Akku
+//     }
+// }
+
+
+public class main {
+    public static void printJava() {
+        System.out.println("Hello Java");
+    }
+    public static void printName(String name){
+        System.out.println(name);
+    }
+    public static void printSum(int a , int b){
+        int sum = a + b;
+        System.out.println(sum);
+    }
+     public static void main(String[] args) {
+        printSum(1, 6); // 7 
+    }
+}
